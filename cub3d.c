4e2d3b4bddf03200df65b0d609297744f3e20bc6
Ayoub Amzouar <gramgeek@gmail.com>
2020-02-07 15:56:51 +0100

    Now The Zome Is Working Well

--- cub3d.c
+++ cub3d.c
@@ -1,12 +1,42 @@
 #include "cub3d.h"
 
-int		rectangle(int key, int color)
+void	rectangle(int color)
 {
 	int midx;
 	int midy;
+	int y;
+	int x;
 
+	x = 0;
 	midx = (wx - rx) / 2;
 	midy = (wy - ry) / 2;
+	while (x < rx)
+	{
+		y = 0;
+		while (y < ry)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, midx + x, midy + y, color);
+			y++;
+		}
+		x++;
+	}
+}
+
+int	zome(int key, void *param)
+{
+	rectangle(0x000000);
+	if (key == 65362)
+	{
+		rx += 10;
+		ry += 10;
+	}
+	else if (key == 65364)
+	{
+		rx -= 10;
+		ry -=10;
+	}
+	rectangle(0x7C05F2);
+	return (0);
 }
 
 int		main(void)
@@ -15,10 +45,11 @@ int		main(void)
 	ry = 240;
 	wx = 1024;
 	wy = 768;
+
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, wx, wy, "cub3d");
-	mlx_key_hook(win_ptr, rectangle, 0xD95284);
-	draw_lines(0xD95284);
+	//draw_lines(0xD95284);
+	mlx_key_hook(win_ptr, zome, (void*)0);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
--- cub3d.h
+++ cub3d.h
@@ -2,12 +2,14 @@
 
 #define CUBED_H
 
+# include <stdio.h>
+# include <mlx.h>
+
 void	*mlx_ptr;
 void	*win_ptr;
-int		wx;
-int		wy;
-int		rx;
-int		ry;
-
+int	wx;
+int	wy;
+int	rx;
+int	ry;
 
 #endif
