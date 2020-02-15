cafb75116c6488d9f3810aed3aeb1314a349220b
Ayoub Amzouar <aamzouar@e1r8p10.1337.ma>
2020-02-15 17:01:33 +0100

    start coding in cub3d.c in redirection functino

--- cub3d.c
+++ cub3d.c
@@ -6,11 +6,20 @@
 /*   By: aamzouar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 15:54:49 by aamzouar          #+#    #+#             */
-/*   Updated: 2020/02/15 16:45:02 by aamzouar         ###   ########.fr       */
+/*   Updated: 2020/02/15 17:00:41 by aamzouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	redirection(char *data_file)
+{
+	int		fd;
+	char	*line;
+
+	if ((fd = open(data_file, O_RDONLY) == -1)
+			ft_putstr_fd(strerror(errno), 1);
+}
 
 int		main(int argc, char *argv[])
 {
@@ -19,5 +28,6 @@ int		main(int argc, char *argv[])
 		ft_putstr_fd("Wrong Number Of Arguments", 1);
 		return (-1);
 	}
+	redirection(argv[1]);
 	return (0);
 }
