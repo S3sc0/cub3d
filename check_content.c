19dde80b5d3456bd15c6c0a868fdd3738a1e1719
Ayoub Amzouar <aamzouar@e1r8p10.1337.ma>
2020-02-16 11:55:05 +0100

    add new file that will check the content of certain line

--- check_content.c
+++ check_content.c
@@ -1,38 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   checks1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aamzouar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/15 15:24:05 by aamzouar          #+#    #+#             */
-/*   Updated: 2020/02/16 11:21:38 by aamzouar         ###   ########.fr       */
+/*   Created: 2020/02/16 11:02:55 by aamzouar          #+#    #+#             */
+/*   Updated: 2020/02/16 11:23:06 by aamzouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
+#include "cub3d.h"
 
-#define CUB3D_H
-#define BUFFER_SIZE 10000
+// the aim of this function is to check if the file has the correct extension
 
-# include <mlx.h>
-# include "libft.h"
-# include <errno.h>
-# include <unistd.h>
-# include <string.h>
-# include <fcntl.h>
-
-typedef struct
+int		correct_exten(char *name)
 {
-	int		wx;
-	int		wy;
-	char	*path[5];
-	int		floor[3];
-	int		cell[3];
-	char	**map;
-}			info;
+	int i;
 
-int			get_next_line(int fd, char **line);
-int			correct_exten(char *name);
-
-#endif
+	i = 0;
+	while (name[i] != '\0')
+	{
+		if (name[i] == '.')
+			if (ft_memcmp(&name[++i], "cub", 3) == 0)
+				return (1);
+			else
+				break ;
+		i++;
+	}
+	return (0);
+}
--- cub3d.c
+++ cub3d.c
@@ -6,26 +6,44 @@
 /*   By: aamzouar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 15:54:49 by aamzouar          #+#    #+#             */
-/*   Updated: 2020/02/15 17:00:41 by aamzouar         ###   ########.fr       */
+/*   Updated: 2020/02/16 11:54:17 by aamzouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+// this function is willing to redirect the given line to the right function which will store it in the right buffer
 
 void	redirection(char *data_file)
 {
 	int		fd;
 	char	*line;
 
-	if ((fd = open(data_file, O_RDONLY) == -1)
-			ft_putstr_fd(strerror(errno), 1);
+	if (correct_exten(data_file) == 0 || (fd = open(data_file, O_RDONLY) == -1))
+	{
+		perror("Error: "); // if file's extension is not correct get out
+		exit(0);
+	}
+	while (get_next_line(fd, &line) == 1)
+	{
+		if (ft_memcmp(line, "R ", 2) == 0) // checks resolution data
+			store_resolution(line);
+		else if (ft_memcmp(line, "F ", 2) == 0 || ft_memcmp(line, "C ", 2) == 2) // checks floor and cell data
+			store_rgb_color(line);
+		else if (check_path == 1) // check the path of the texture
+			store_path(line);
+		else if (ft_memcmp(line, "\n", 1) == 1) // check if there a new line
+			continue ;
+		else	
+		free(line);
+	}
 }
 
 int		main(int argc, char *argv[])
 {
 	if (argc != 2 || argc != 3)
 	{
-		ft_putstr_fd("Wrong Number Of Arguments", 1);
+		ft_putstr_fd("Wrong Number Of Arguments", 1); // error if there is more or less then required number of arguments
 		return (-1);
 	}
 	redirection(argv[1]);
