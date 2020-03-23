d1b7330015c592dcb64d932172aa849ca6b28d1e
ayoub amzouar <gramgeek01@gmail.com>
2020-03-23 21:00:38 +0100

    still workin on sprite i have problem on x scaling

--- sprite_rendering.c
+++ sprite_rendering.c
@@ -18,8 +18,6 @@ void	sprite_dst(player plr, rycrd hi, rycrd vi, float ray_angle)
 	float	res1;
 	float	res2;
 
-	sprt.x = 0;
-	sprt.y = 0;
 	g_asprite = 1;
 	if (hi.sx == 0 && hi.sy == 0 && vi.sx == 0 && vi.sy == 0)
 	{
@@ -28,7 +26,7 @@ void	sprite_dst(player plr, rycrd hi, rycrd vi, float ray_angle)
 	}
 	res1 = sqrt(pow(plr.x - hi.sx, 2) + pow(plr.y - hi.sy, 2));
 	res2 = sqrt(pow(plr.x - vi.sx, 2) + pow(plr.y - vi.sy, 2));
-	if (((vi.sx == 0 && vi.sy == 0) || res1 < res2) && hi.sx != 0 && hi.sy != 0)
+	if (res1 < res2)
 	{
 		sprt.x = hi.sx;
 		sprt.y = hi.sy;
@@ -42,18 +40,8 @@ void	sprite_dst(player plr, rycrd hi, rycrd vi, float ray_angle)
 	}
 }
 
-// just calc the distance between two points
-float	mini_dst(player plr, float ray_angle, crd cntr)
-{
-	float	res;
-
-	res = sqrt(pow(plr.x - cntr.x, 2) + pow(plr.y - cntr.y, 2));
-	//res *= cos((ray_angle - plr.rotationA) * RADIN);
-	return (res);
-}
-
 // it calcs the final destance of between the player and the sprite
-float	dst_to_sprite(player plr, float ray_angle)
+float	dst_to_sprite(player plr, float ray_angle, data info)
 {
 	crd	point;
 	float	r;
@@ -61,50 +49,45 @@ float	dst_to_sprite(player plr, float ray_angle)
 	int	i1;
 	int	i2;
 
-	i1 = g_vert == 1 && ray_angle >= 90 && ray_angle <= 270 ? -1 : 1;
-	i2 = g_vert == 0 && ray_angle >= 180 && ray_angle <= 360 ? -1 : 1;
+	i1 = g_vert == 1 && ray_angle > 90 && ray_angle < 270 ? -1 : 1;
+	i2 = g_vert == 0 && ray_angle > 180 && ray_angle < 360 ? -1 : 1;
 	// calc sprite center
 	point.x = ((int)(sprt.x / SQUARE_SIZE) * SQUARE_SIZE) + (SQUARE_SIZE / 2) * i1;
 	point.y = ((int)(sprt.y / SQUARE_SIZE) * SQUARE_SIZE) + (SQUARE_SIZE / 2) * i2;
 	// calc the destance between the player and the center
-	r = mini_dst(plr, ray_angle, point);
+	r = sqrt(pow(plr.x - point.x, 2) + pow(plr.y - point.y, 2));
 	// calc the intersection points
-	point.x = plr.x + cos(ray_angle * RADIN) * r;
-	point.y = plr.y + sin(ray_angle * RADIN) * r;
-	sprt.x = point.x;
-	sprt.y = point.y;
-	g_offset_s = g_vert == 0 ? (int)point.x % SQUARE_SIZE : (int)point.y % SQUARE_SIZE;
+	sprt.x = plr.x + cos(ray_angle * RADIN) * r;
+	sprt.y = plr.y + sin(ray_angle * RADIN) * r;
+	g_offset_s = (int)sprt.y % SQUARE_SIZE;
 	// calc the wanted destance, voila !!!
-	dst = mini_dst(plr, ray_angle, point);
-	return (dst);
-}
+	dst = sqrt(pow(plr.x - sprt.x, 2) + pow(plr.y - sprt.y, 2));
+
 
 // this function will render my sprite
 void	sprite_rendering(player plr, float ray_angle, data info, int x)
 {
-	float	dst;
-	wall	s;
-	float	tmp;
-	int	y;
-	int	i;
+	float		dst;
+	wall		s;
+	float		tmp;
+	int		i;
 
-	dst = dst_to_sprite(plr, ray_angle);
+	dst = dst_to_sprite(plr, ray_angle, info);
 	s.dpp = (info.wx / 2) / tan((FOV_ANGLE * RADIN) / 2);
 	s.bottom = (SQUARE_SIZE / dst) * s.dpp;
 	tmp = s.bottom;
 	s.bottom = s.bottom > info.wy ? info.wy : s.bottom;
 	s.top = (info.wy - s.bottom) / 2;
-	y = s.top;
 	i = 0;
 	if (info.the_map[(int)floor(sprt.y / SQUARE_SIZE)][(int)floor(sprt.x / SQUARE_SIZE)] == '2')
 	{
 		while (i < s.bottom)
 		{
-			s.dst_ftop = y + (tmp / 2) - (info.wy / 2);
+			s.dst_ftop = s.top + (tmp / 2) - (info.wy / 2);
 			s.offset_y = s.dst_ftop * (64.0 / tmp);
 			if (info.S[64 * s.offset_y + g_offset_s] != 0)
-				g_img_data[x + y * info.wx] = info.S[64 * s.offset_y + g_offset_s];
-			y++;
+				g_img_data[s.top * info.wx + x] = info.S[64 * s.offset_y + g_offset_s];
+			s.top++;
 			i++;
 		}
 	}
